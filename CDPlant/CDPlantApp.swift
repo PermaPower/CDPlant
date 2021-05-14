8439de1336aeafaec3f5510b26f2a464bdb88da3
David Power <dpower19@gmail.com>
2021-05-14 14:46:55 +1000

    Initial Commit

---
+++ CDPlant/CDPlantApp.swift
@@ -0,0 +1,17 @@
+//
+//  CDPlantApp.swift
+//  CDPlant
+//
+//  Created by David Power on 14/5/21.
+//
+
+import SwiftUI
+
+@main
+struct CDPlantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CDPlant/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CDPlant
+//
+//  Created by David Power on 14/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
